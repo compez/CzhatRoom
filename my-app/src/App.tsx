d2228656869fa6b9eade57b1290e63242dc6a3a4
compez <czms1208@gmail.com>
2019-04-22 23:35:56 -0700

    front-end begins here

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIOClient from 'socket.io-client'
 
-class App extends Component {
+interface MyState {
+  isLoaded: boolean,
+  name: string,
+  message: string,
+  chattingWindow: Array<{name: string, message: string}>
+}
+
+class App extends Component<{}, MyState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      name: "",
+      message: "",
+      chattingWindow: [{name: "test: ", message: "Oh Hi Mark"}]
+    }
+  }
+
+  componentDidMount() {
+    const socket = socketIOClient("http://localhost:8080");
+    socket.on('chat', (data: any) => {
+      console.log(data);
+      let newChatting = this.state.chattingWindow.concat(data);
+      this.setState({chattingWindow: newChatting})
+    });
+  }
+
+  emitHandler(key: string,) {
+    if (key != 'Enter') return;
+    const socket = socketIOClient("http://localhost:8080");
+    socket.emit('chat', {name: this.state.name, message: this.state.message});
+    this.setState({message: ""});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="show">
+        <p>messages go here</p>
+        {this.state.chattingWindow.map((a, index) => 
+          <p key={index}>{a.name + ": " + a.message}</p>
+        )}
+
+        </div>
+        <div>
+          <input placeholder="Enter your name here" value={this.state.name} onChange={(e) => {this.setState({name: e.target.value})}} />
+        </div>
+        <div>
+          <input placeholder="type here" value={this.state.message} onChange={(e) => {this.setState({message: e.target.value})}} onKeyDown={(e) => {this.emitHandler(e.key)}}/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
